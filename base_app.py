58d2059a0e7a45c44f52b46dd4751f1d803a668f
Tristan Krafft <tristankrafft08@gmail.com>
2022-12-07 12:47:34 +0200

    adding multiple models

--- base_app.py
+++ base_app.py
@@ -28,6 +28,13 @@ import joblib,os
 # Data dependencies
 import pandas as pd
 
+[theme]
+primaryColor = "#E694FF"
+backgroundColor = "#00172B"
+secondaryBackgroundColor = "#0083B8"
+textColor = "#C6CDD4"
+font = "sans-serif"
+
 # Vectorizer
 news_vectorizer = open("resources/Vectoriser.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
@@ -46,7 +53,7 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Log regression","SVM","SMOTE", "Information"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
@@ -60,7 +67,7 @@ def main():
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
 	# Building out the predication page
-	if selection == "Prediction":
+	if selection == "Log regression":
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
@@ -76,16 +83,24 @@ def main():
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
-			if prediction == 1:
-				return "pro"
-			if prediction == -1:
-				return "anti"
-			if prediction == 0:
-				return "neutral"
-			if prediction == 2:
-				return "news"
+			st.success("Text Categorized as: {}".format(prediction)) 
 
+	if selection == "SVM":
+		st.info("Prediction with ML Models")
+		# Creating a text box for user input
+		tweet_text = st.text_area("Enter Text","Type Here")
 
+		if st.button("Classify"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/SVM.pkl"),"rb"))
+			prediction = predictor.predict(vect_text)
+
+			# When model has successfully run, will print prediction
+			# You can use a dictionary or similar structure to make this output
+			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction)) 
 
 # Required to let Streamlit instantiate our web app.  
