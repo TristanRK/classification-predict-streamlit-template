62e146dde172bd20dbff36b4c3e279e8260625c7
Tristan Krafft <tristankrafft08@gmail.com>
2022-12-08 14:18:17 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -28,7 +28,7 @@ import joblib,os
 
 from PIL import Image
 # Loading Image using PIL
-im = Image.open('/content/image.jpg')
+im = Image.open('content/image.jpg')
 # Adding Image to web app
 st.set_page_config(page_title="base_app.py", page_icon = im)
 
