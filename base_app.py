6f7ae27053e5ccecfa2c73a81dcbbc34b0947adc
Tristan Krafft <tristankrafft08@gmail.com>
2022-12-09 22:58:18 +0200

    adding sub header to the data info

--- base_app.py
+++ base_app.py
@@ -85,6 +85,7 @@ def main():
 	if selection == "Project Information":
 		st.info("Many companies are built around lessening one’s environmental impact or carbon footprint. They offer products and services that are environmentally friendly and sustainable, in line with their values and ideals. They would like to determine how people perceive climate change and whether or not they believe it is a real threat. This would add to their market research efforts in gauging how their product/service may be received. With this context, EDSA throwing a challeng with the task of creating a Machine Learning model that is able to classify whether or not a person believes in climate change, based on their novel tweet data. Providing an accurate and robust solution to this task gives companies access to a broad base of consumer sentiment, spanning multiple demographic and geographic categories - thus increasing their insights and informing future marketing strategies")
 		# You can read a markdown file from supporting resources folder
+		st.subheader("Where is our data from?")
 		st.markdown("The collection of this data was funded by a Canada Foundation for Innovation JELF Grant to Chris Bauch, University of Waterloo. The dataset aggregates tweets pertaining to climate change collected between Apr 27, 2015 and Feb 21, 2018. In total, 43,943 tweets were collected. Each tweet is labelled as one of 4 classes, which are described below.")
 
 		st.subheader("Raw Twitter data and sentiment labels")
