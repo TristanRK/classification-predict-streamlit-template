8a26cb9366e52093c219b00874014c958d5d6d6c
Tristan Krafft <tristankrafft08@gmail.com>
2022-12-13 10:56:01 +0200

    putting logo in center

--- base_app.py
+++ base_app.py
@@ -44,6 +44,8 @@ st.markdown(hide_default_format, unsafe_allow_html=True)
 # Data dependencies
 import pandas as pd
 
+st.image('content/Logo2.png', width=100)
+
 col1, col2, col3 = st.columns([1,1,1])
 with col1:
 	st.write(" ")
