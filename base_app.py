8eed61d9d73708e7a5ccdbb718d8ad1d8387a340
Tristan Krafft <tristankrafft08@gmail.com>
2022-12-13 21:44:40 +0200

    updating model page

--- base_app.py
+++ base_app.py
@@ -82,7 +82,7 @@ def main():
 		st.image("https://th.bing.com/th/id/R.ecbb577764245551f2eb3d68db207808?rik=7z5rKX4dMPIj0g&riu=http%3a%2f%2fworld.350.org%2fnz%2ffiles%2f2014%2f01%2fTwitter-350.jpg&ehk=JjB3BnuqD6vhFaspJVVLSwEKtbPCPc3rwUfywG77Rp4%3d&risl=&pid=ImgRaw&r=0",width = 800)
 		
 		
-		st.info("Many companies are built around lessening one’s environmental impact or carbon footprint. They offer products and services that are environmentally friendly and sustainable, in line with their values and ideals. They would like to determine how people perceive climate change and whether or not they believe it is a real threat. This would add to their market research efforts in gauging how their product/service may be received. With this context, EDSA throwing a challeng with the task of creating a Machine Learning model that is able to classify whether or not a person believes in climate change, based on their novel tweet data. Providing an accurate and robust solution to this task gives companies access to a broad base of consumer sentiment, spanning multiple demographic and geographic categories - thus increasing their insights and informing future marketing strategies")
+		st.info("Many companies are built around lessening one’s environmental impact or carbon footprint. They offer products and services that are environmentally friendly and sustainable, in line with their values and ideals. They would like to determine how people perceive climate change and whether or not they believe it is a real threat. This would add to their market research efforts in gauging how their product/service may be received. With this context, EDSA is throwing a challeng with the task of creating a Machine Learning model that is able to classify whether or not a person believes in climate change. This would be based on their novel tweet data. Providing an accurate and robust solution to this task gives companies access to a broad base of consumer sentiment, spanning multiple demographic and geographic categories - thus increasing their insights and informing future marketing strategies.")
 		# You can read a markdown file from supporting resources folder
 		st.subheader("Where is our data from?")
 		st.markdown("The collection of this data was funded by a Canada Foundation for Innovation JELF Grant to Chris Bauch, University of Waterloo. The dataset aggregates tweets pertaining to climate change collected between Apr 27, 2015 and Feb 21, 2018. In total, 43,943 tweets were collected. Each tweet is labelled as one of 4 classes, which are described below.")
@@ -116,7 +116,7 @@ def main():
 
 		st.info("EnviroData is an environmentally focused data science consultancy founded in 2022 by four innovative individuals. These four individuals are Farayi, Solomon, Tristan and Peakanyo. As a data science consultancy, we aim to provide insights of what makes the world tick and more importantly how environmental issues are dealt with in our modern society.")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
+		
 
 		st.subheader("Amazing People")
 
@@ -153,9 +153,8 @@ def main():
 
 		# Building out the predication page
 		if selectmodel == "Logistic Regression":
-			st.info("Prediction with ML Models")
 			# Creating a text box for user input
-			tweet_text = st.text_area("Enter Text","Type Here")
+			tweet_text = st.text_area("Can make prediction with ML Models with text box below","Type Here")
 
 			if st.button("Classify"):
 				# Transforming user input with vectorizer
@@ -167,13 +166,13 @@ def main():
 
 				predicted = []
 				if prediction == 1:
-					predicted = "pro"
+					predicted = "Pro belief in climate change"
 				if prediction == -1:
-					predicted = "anti"
+					predicted = "Anti belief in climate change"
 				if prediction == 0:
-					predicted = "neutral"
+					predicted = "Neutral in belief of climate change"
 				if prediction == 2:
-					predicted = "news"
+					predicted = "News related tweets"
 
 				# When model has successfully run, will print prediction
 				# You can use a dictionary or similar structure to make this output
@@ -195,13 +194,13 @@ def main():
 
 				predicted = []
 				if prediction == 1:
-					predicted = "pro"
+					predicted = "Pro belief in climate change"
 				if prediction == -1:
-					predicted = "anti"
+					predicted = "Anti belief in climate change"
 				if prediction == 0:
-					predicted = "neutral"
+					predicted = "Neutral in belief of climate change"
 				if prediction == 2:
-					predicted = "news"
+					predicted = "News related tweets"
 
 				# When model has successfully run, will print prediction
 				# You can use a dictionary or similar structure to make this output
@@ -223,13 +222,13 @@ def main():
 
 				predicted = []
 				if prediction == 1:
-					predicted = "pro"
+					predicted = "Pro belief in climate change"
 				if prediction == -1:
-					predicted = "anti"
+					predicted = "Anti belief in climate change"
 				if prediction == 0:
-					predicted = "neutral"
+					predicted = "Neutral in belief of climate change"
 				if prediction == 2:
-					predicted = "news"
+					predicted = "News related tweets"
 			
 
 				# When model has successfully run, will print prediction
@@ -253,13 +252,13 @@ def main():
 
 				predicted = []
 				if prediction == 1:
-					predicted = "pro"
+					predicted = "Pro belief in climate change"
 				if prediction == -1:
-					predicted = "anti"
+					predicted = "Anti belief in climate change"
 				if prediction == 0:
-					predicted = "neutral"
+					predicted = "Neutral in belief of climate change"
 				if prediction == 2:
-					predicted = "news"
+					predicted = "News related tweets"
 			
 
 				# When model has successfully run, will print prediction
@@ -282,13 +281,13 @@ def main():
 
 				predicted = []
 				if prediction == 1:
-					predicted = "pro"
+					predicted = "Pro belief in climate change"
 				if prediction == -1:
-					predicted = "anti"
+					predicted = "Anti belief in climate change"
 				if prediction == 0:
-					predicted = "neutral"
+					predicted = "Neutral in belief of climate change"
 				if prediction == 2:
-					predicted = "news"
+					predicted = "News related tweets"
 			
 
 				# When model has successfully run, will print prediction
@@ -311,13 +310,13 @@ def main():
 
 				predicted = []
 				if prediction == 1:
-					predicted = "pro"
+					predicted = "Pro belief in climate change"
 				if prediction == -1:
-					predicted = "anti"
+					predicted = "Anti belief in climate change "
 				if prediction == 0:
-					predicted = "neutral"
+					predicted = "Neutral in belief of climate change"
 				if prediction == 2:
-					predicted = "news"
+					predicted = "News related tweets"
 			
 
 				# When model has successfully run, will print prediction
